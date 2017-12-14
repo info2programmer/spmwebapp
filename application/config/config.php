c43a26dbe23eb5d7e7ad688ed6a6f05e686f10a2
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2017-12-14 10:45:48 +0530

    Config File Update

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://spmgroupindia.com/propayroll';
+// $config['base_url'] = 'http://spmgroupindia.com/propayroll';
+$config['base_url'] = 'http://localhost/spm/';
 
 
 /*
